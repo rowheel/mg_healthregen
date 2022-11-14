17e3a4964a9010cf24878d9be265fedb744860e9
Roberta Wheeler <rob3rt.233@gmail.com>
2022-11-14 21:12:11 +0800

    Second commit

--- lua/autorun/server/healthregen.lua
+++ lua/autorun/server/healthregen.lua
@@ -2,12 +2,30 @@
 -- Registers a hook that regenerates player health like Modern Warfare
 -- Copyleft 2022 Garry's Mod Player. All rights reversed.
 
+-- Parameter player is a reference to an online player
+-- Returns the timer identifier for that player
+local function getTimerIdentifier(player)
+	return "healthregen: " .. player:UserID()
+end
+
+-- Parameter player is a reference to an online player
+-- Returns true is that player can regenerate health, otherwise false
+local function canRegenerateHealth(player)
+	return player:Alive() and player:Health() < player:GetMaxHealth()
+end
+
 -- Registers a hook that regenerates player health like Modern Warfare
 -- Returns nil
 hook.Add("PlayerHurt", "healthregen", function(victim)
-	timer.Create("healthregen: " .. victim:UserID(), 10, 1, function()
-		if victim:GetHealth() < victim:GetMaxHealth() then
+	timer.Create(getTimerIdentifier(victim), 10, 1, function()
+		if canRegenerateHealth(victim) then
 			victim:SetHealth(victim:GetMaxHealth())
 		end
 	end)
+end)
+
+-- Registers a hook that removes the timer after player death
+-- Returns nil
+hook.Add("PostPlayerDeath", "remove_healthregen", function(player)
+	timer.Remove(getTimerIdentifier(player))
 end)
