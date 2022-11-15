8d8647130eb09bc943e8616275368ba6d2c2c0bb
Roberta Wheeler <rob3rt.233@gmail.com>
2022-11-15 10:20:37 +0800

    Update function name and docstring

--- lua/autorun/server/healthregen.lua
+++ lua/autorun/server/healthregen.lua
@@ -3,20 +3,20 @@
 -- Copyleft 2022 Garry's Mod Player. All rights reversed.
 
 -- Parameter player is a reference to an online player
--- Returns the timer identifier for that player
-local function getTimerIdentifier(player)
+-- Returns a unique timer identifier for that player
+local function getIdentifier(player)
 	return "healthregen: " .. player:UserID()
 end
 
 -- Parameter player is a reference to an online player
--- Returns the timer delay for that player
-local function getTimerDelay(player)
+-- Returns current regeneration delay for that player
+local function getDelay(player)
 	return math.ceil((player:GetMaxHealth() - player:Health()) / 20) + 5
 end
 
 -- Parameter player is a reference to an online player
 -- Returns true if that player can regenerate health, otherwise false
-local function canRegenerateHealth(player)
+local function canRegenerate(player)
 	return player:Alive() and player:Health() < player:GetMaxHealth()
 end
 
@@ -24,12 +24,12 @@ end
 -- Parameter player is a reference to an online player
 -- Returns nil
 hook.Add("PlayerHurt", "create_healthregen", function(player)
-	if not canRegenerateHealth(player) then
-		return timer.Remove(getTimerIdentifier(player))
+	if not canRegenerate(player) then
+		return timer.Remove(getIdentifier(player))
 	end
 
-	timer.Create(getTimerIdentifier(player), getTimerDelay(player), 1, function()
-		if canRegenerateHealth(player) then
+	timer.Create(getIdentifier(player), getDelay(player), 1, function()
+		if canRegenerate(player) then
 			player:SetHealth(player:GetMaxHealth())
 		end
 	end)
@@ -38,5 +38,5 @@ end)
 -- Registers a hook that removes the timer after player death
 -- Parameter player is a reference to an online player
 hook.Add("PostPlayerDeath", "remove_healthregen", function(player)
-	timer.Remove(getTimerIdentifier(player))
+	timer.Remove(getIdentifier(player))
 end)
